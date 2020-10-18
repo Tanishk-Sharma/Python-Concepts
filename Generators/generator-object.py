cd6829275c5e92d29dd880e562c5a3e95c53a58e
Tanishk Sharma <tanishk9597@gmail.com>
2020-10-18 20:13:20 +0530

    Update generator-object.py

--- Generators/generator-object.py
+++ Generators/generator-object.py
@@ -1,1 +1,7 @@
-TODO
+list_of_any_numbers = [7, 1, 5, 9, 8]
+g = (i**2 for i in list_of_any_numbers)  # generator expression
+print(next(g))         # -> 49
+print(next(g))         # -> 1
+print(next(g))         # -> 25
+print(next(g))         # -> 81
+print(next(g))         # -> 64
