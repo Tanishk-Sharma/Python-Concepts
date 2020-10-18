a09558f4f9d11dda782081e305c5c1d3d4af2883
Tanishk Sharma <tanishk9597@gmail.com>
2020-10-18 19:09:31 +0530

    Update iterator.py

--- Iterators/iterator.py
+++ Iterators/iterator.py
@@ -5,14 +5,14 @@ list_of_natural_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 natural_numbers_iterator = iter(list_of_natural_numbers)
 
 # Lets print the elements of the collection (List) using the iterator object and its method - next(ele)
-print(next(s))      # -> 1
-print(next(s))      # -> 2
-print(next(s))      # -> 3
-print(next(s))      # -> 4
-print(next(s))      # -> 5
-print(next(s))      # -> 6
-print(next(s))      # -> 7
-print(next(s))      # -> 8
-print(next(s))      # -> 9
-print(next(s))      # -> 10
-print(next(s))      # -> We have exhausted out list hence, at this point the program will raise the 'StopIteration Error'
+print(next(natural_numbers_iterator))      # -> 1
+print(next(natural_numbers_iterator))      # -> 2
+print(next(natural_numbers_iterator))      # -> 3
+print(next(natural_numbers_iterator))      # -> 4
+print(next(natural_numbers_iterator))      # -> 5
+print(next(natural_numbers_iterator))      # -> 6
+print(next(natural_numbers_iterator))      # -> 7
+print(next(natural_numbers_iterator))      # -> 8
+print(next(natural_numbers_iterator))      # -> 9
+print(next(natural_numbers_iterator))      # -> 10
+print(next(natural_numbers_iterator))      # -> We have exhausted out list hence, at this point the program will raise the 'StopIteration Error'
