d897742a68704924a9c1240be9a89634594c79a4
Tanishk Sharma <tanishk9597@gmail.com>
2020-10-20 18:03:31 +0530

    Update regex.py

--- Regular Expressions/regex.py
+++ Regular Expressions/regex.py
@@ -1,1 +1,21 @@
-TODO
+import re
+
+my_str = """
+Hi There!
+Here are our contact details:
+
+If you would like to contact me, please send a mail to someusername@somedomain.com
+If you want to get in touch with Sales, please mail to salesusername@somedomain.com
+Our vendors would be happy to respond on vendormail@vendordomain.net
+Thanks for your interest!
+Great Guy,
+Some Company,
+companyaddress@companydomain.xyz
+"""
+
+mail_pattern = r'[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]{3}'
+
+all_mails_from_text = re.findall(mail_pattern, my_str)
+
+for mail_id in all_mails_from_text:
+  print(mail_id)
